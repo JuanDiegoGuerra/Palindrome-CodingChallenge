4bf78f3ada10bc62c4b79ca3e498356638c40c24
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-06 15:05:10 -0500

    Challenge Solution

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,2 +1,6 @@
 module.exports = function (str) {
+    perfectdrome = str.toLowerCase().replace(/([^\w_]+|\s+)/g, '');
+    perfectdrome = perfectdrome.replace('_', '');
+    let reversestr = perfectdrome.split('').reverse().join(''); 
+    return reversestr === perfectdrome;
 }
